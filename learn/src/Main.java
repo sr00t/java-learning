1c6d32920d80486d81717b11114d9482068b37ad
mashour <mslam.ashour@ionos.com>
2023-04-05 11:51:53 +0200

    hello world

--- learn/src/Main.java
+++ learn/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         for (int i = 0; i <= 10; i ++) {
-            System.out.println("fuck u " + i);
+            System.out.println("hello world!" + i);
         }
     }
 }
