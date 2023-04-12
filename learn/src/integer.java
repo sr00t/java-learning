a7ccf5bbda853a50eec194fb742df54cf85b2ca3
mashour <mslam.ashour@ionos.com>
2023-04-12 11:24:48 +0200

    int

---
+++ learn/src/integer.java
@@ -0,0 +1,11 @@
+public class integer {
+
+    //how to use int
+    public static void main(String[] args){
+            int f, d, g;
+            f = 10;
+            d = 20;
+            g = f + d;
+    System.out.println("test is "+ g);
+        }
+}
