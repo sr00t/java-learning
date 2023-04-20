e01391678c0dbef789270424159f8474c61e5fc2
mashour <mslam.ashour@ionos.com>
2023-04-20 19:42:00 +0200

    blue print katze

---
+++ learn/src/Aufgaben/katze.java
@@ -0,0 +1,10 @@
+package Aufgaben;
+
+public class katze {
+    int atler = 1;
+    int gewicht = 132;
+    String name = "cat";
+    String farbe = "color";
+    int rasse = 8;
+    String faehigkeit = "miauu";
+}
